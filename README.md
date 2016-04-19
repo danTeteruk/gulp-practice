0fcc395c3d500b53495d45230ca2464d44bfccc2
danteteruk <danuloteteruk@gmail.com>
2016-04-19 04:20:04 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,12 @@
+#How to install
+
+- npm install
+- bower install
+
+#How to run with watchers or just build
+
+- gulp serve (run browser-sync and watchers)
+
+- gulp build (build src)
+
+- gulp (build and serve)
---
+++ gulpfile.js
@@ -0,0 +1,101 @@
+var gulp = require('gulp');
+var gconcat = require('gulp-concat');
+var cssmin = require('gulp-cssmin');
+var htmlmin = require('gulp-htmlmin');
+var mainBowerFiles = require('main-bower-files');
+var uglify = require('gulp-uglify');
+var filter = require('gulp-filter');
+var imagemin = require('gulp-imagemin');
+var browserSync = require('browser-sync').create();
+var del = require('del');
+var mainBowerFiles = require('main-bower-files');
+var sass = require('gulp-sass');
+var git = require('gulp-git');
+
+
+var pth = {
+   src: 'src',
+   build: 'build'
+};
+
+
+gulp.task('add', function(){
+  return gulp.src("*")
+    .pipe(git.add());
+});
+
+gulp.task('commit', function(){
+  return gulp.src('*')
+    .pipe(git.commit('one of many commit'));
+});
+
+gulp.task('push', function(){
+  git.push('origin', 'master', function (err) {
+    if (err) throw err;
+  });
+});
+
+
+gulp.task('bower', function() {
+    var cssFilter = filter('**/*.css');
+    return gulp.src(mainBowerFiles())
+        .pipe(cssFilter)
+        .pipe(gconcat('vendor.css'))
+        .pipe(cssmin())
+        .pipe(gulp.dest(pth.build + '/css'))
+});
+
+gulp.task('sass', function () {
+  return gulp.src(pth.src + '/scss/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gconcat('main.css'))
+    .pipe(cssmin())
+    .pipe(gulp.dest(pth.build + '/css'))
+});
+
+gulp.task('html', function() {
+   return gulp.src(pth.src + '/**/*.html')
+       .pipe(htmlmin({collapseWhitespace: true}))
+       .pipe(gulp.dest(pth.build))
+});
+
+gulp.task('images', function() {
+   return gulp.src(pth.src + '/img/**/*')
+       .pipe(imagemin({progressive: true}))
+       .pipe(gulp.dest(pth.build + '/img'))
+});
+
+gulp.task('watch', ['browser-sync'], function(){
+   gulp.watch(pth.src + '/scss/**/*', ['bsync:sass']);
+   gulp.watch(pth.src + '/**/*.html', ['bsync:html']);
+   gulp.watch(pth.src + '/js/**/*', ['bsync:scripts']);
+});
+
+gulp.task('bsync:sass', ['sass'], function(){
+   browserSync.reload();
+});
+
+gulp.task('bsync:html', ['html'], function(){
+   browserSync.reload();
+});
+
+
+gulp.task('browser-sync', ['sass', 'html', 'images'], function() {
+   browserSync.init({
+      server: {
+         baseDir: "./build"
+      }
+   });
+});
+
+gulp.task('clean', function(){
+   return del('build');
+});
+
+gulp.task('build', ['bower', 'sass', 'html', 'images']);
+
+gulp.task('serve', ['browser-sync', 'watch']);
+
+gulp.task('default', ['build', 'serve']);
+
+gulp.task('pushToMaster', ['add', 'commit', 'push']);
